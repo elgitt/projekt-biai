0b1f1707eb4b1de2d09addbce3959c6a989e191b
magddzi881 <72300027+magddzi881@users.noreply.github.com>
2023-03-26 17:50:49 +0200

    update

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,10 +1,25 @@
-import pandas as pd 
+import pandas as pd
+import pyarrow as pr
+import fastparquet as fpq
 
-def print_hi(name):
-    print(f'Hi, {name}')
+with open(r"data/0kg_L.csv", 'r') as fp:
+    for count, line in enumerate(fp):
+        pass
+#print('Total Lines', count + 1)
 
+Okg_L = pd.read_csv('data/0kg_L.csv', skiprows= 21, low_memory=False, nrows=19000, header=2)
+Okg_R = pd.read_csv('data/0kg_R.csv', skiprows= 21, low_memory=False, nrows=19000, header=2)
+Okg_W = pd.read_csv('data/0kg_W.csv', skiprows= 21, low_memory=False, nrows=19000, header=2)
+Ikg_L = pd.read_csv('data/1kgL_L.csv', skiprows= 21, low_memory=False, nrows=19000, header=2)
+Ikg_R = pd.read_csv('data/1kgL_R.csv', skiprows= 21, low_memory=False, nrows=19000, header=2)
+Ikg_W = pd.read_csv('data/1kgL_W.csv', skiprows= 21, low_memory=False, nrows=19000, header=2)
 
-if __name__ == '__main__':
-    print_hi('ewa')
+Okg_L.columns = ['czas','ch1']
+Okg_R.columns = ['czas','ch1']
+Okg_W.columns = ['czas','ch1']
+Ikg_L.columns = ['czas','ch1']
+Ikg_R.columns = ['czas','ch1']
+Ikg_W.columns = ['czas','ch1']
 
-
+test = Okg_L.to_parquet()
+print(Okg_L)
