7f07c20f459f4194d51360f1c6f9a754c06d1ec3
elgitt <72296432+elgitt@users.noreply.github.com>
2023-03-30 12:43:39 +0200

    update: decymacja

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -21,5 +21,22 @@ Ikg_L.columns = ['czas','ch1']
 Ikg_R.columns = ['czas','ch1']
 Ikg_W.columns = ['czas','ch1']
 
+#wektory z ch1
+Okg_L_vec = Okg_L['ch1']
+Okg_R_vec = Okg_R['ch1']
+Okg_W_vec = Okg_W['ch1']
+Ikg_W_vec = Ikg_W['ch1']
+Ikg_L_vec = Ikg_L['ch1']
+Ikg_R_vec = Ikg_R['ch1']
+
+#decymacja sygnału
+Okg_L_decimated = Okg_L_vec[::100]
+Okg_W_decimated = Okg_W_vec[::100]
+Okg_R_decimated = Okg_R_vec[::100]
+Ikg_W_decimated = Ikg_W_vec[::100]
+Ikg_L_decimated = Ikg_L_vec[::100]
+Ikg_R_decimated = Ikg_R_vec[::100]
+
 test = Okg_L.to_parquet()
-print(Okg_L)
+print(Okg_L_decimated)
+
