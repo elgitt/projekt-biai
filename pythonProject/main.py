a90a68e04c07507ceb7a2b7c4b1bde846ead2adb
magddzi881 <72300027+magddzi881@users.noreply.github.com>
2023-04-01 14:56:17 +0200

    update

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,13 +1,19 @@
 import methods as m
 
 
-test = m.refactor_binary_to_data_frame('0kg_L')
-x1, y1 = m.change_to_vector(test)
+test = m.read_from_parquet('test')
+m5 = m.median_method(test, 5)
+m9 = m.median_method(test, 9)
+m11 = m.median_method(test, 11)
+m15 = m.median_method(test, 15)
+m21 = m.median_method(test, 21)
+
+x1, y1 = m.change_to_vector(m5)
 vectors = []
 for i in range(len(x1)):
     vec = [x1[i], y1[i]]
     vectors.append(vec)
-
+    print(vec)
 
 
 
--- pythonProject/methods.py
+++ pythonProject/methods.py
@@ -3,6 +3,7 @@ import pickle
 import os
 from itertools import islice
 import pandas as pd
+import pyarrow.parquet as pq
 
 
 def convert_csv_to_bin(input_file: str, output_file: str):
@@ -18,23 +19,39 @@ def convert_csv_to_bin(input_file: str, output_file: str):
         bf.close()
 
 
-def refactor_binary_to_data_frame(filepath: str):
+def save_csv_as_parquet(filepath: str):
     with open('data/' + filepath + '.csv') as csvfile:
         reader = csv.reader(islice(csvfile, 22, None))
         data = [row for row in reader]
 
     df = pd.DataFrame(data, columns=['Czas', 'CH1'])
-    window_size = 3
-    rolling_mean = df['Czas'].rolling(window_size).mean()
-
-    # utwórz nową kolumnę w DataFrame z wynikiem filtrowania z przesuwnym oknem
-    df['Sr'] = rolling_mean
-    print(df)
     df.to_parquet('data/test.parquet', compression='snappy', engine='pyarrow', row_group_size=10000)
     return df
 
 
+def read_from_parquet(filepath):
+    table = pq.read_table('data/' + filepath + '.parquet')
+    df = table.to_pandas()
+    return df
+
+
+def median_method(df, window_size):
+    rolling_mean = df['Czas'].rolling(window_size).mean()
+    df['Sr'] = rolling_mean
+    return df
+
+
 def change_to_vector(df):
-    x = df['Czas'].to_numpy()
+    x = df['Sr'].to_numpy()
     y = df['CH1'].to_numpy()
     return x, y
+
+
+def x(file_name):
+    test = save_csv_as_parquet(file_name)
+    m5 = median_method(test, 5)
+    m9 = median_method(test, 9)
+    m11 = median_method(test, 11)
+    m15 = median_method(test, 15)
+    m21 = median_method(test, 21)
+    return {'m5': m5, "m9": m9}
