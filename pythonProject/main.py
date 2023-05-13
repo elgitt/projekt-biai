0d067c0b6602f9d51caaca69f0dc12cb3b48ffdd
magddzi881 <72300027+magddzi881@users.noreply.github.com>
2023-05-13 23:22:07 +0200

    Regresja na wykresie, parquety z pozostalych danych

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -10,46 +10,22 @@ import numpy as np
 # sprawdzić ile jest wartości w każdym z przedziałów (w zasadzie, chodzi o najdłuższy wektor). należy "uzupełnić" dane w każdym z wektorów tak, aby miały takie same długości, np. 500.
 # Przygotowywać dokument, żeby był ślad po tym co robicie.
 
-
 test = m.read_from_parquet('0kg_L')
 
-# m.return_median_mx('0kg_L')['parquet']
-#test2 = m.return_median_mx('0kg_R')['parquet']
-
-#decimated_vec = m.decimate_method(test, 100)
 df_preprocessed = m.preprocess_data(test, decimation_step=10, filter_window_size=20, time_step=2)
 print(df_preprocessed)
 m.regression_model(df_preprocessed)
-#decimated_vec2 = m.decimate_method(test2, 100)
 
-#x1, y1 = m.change_to_vector(decimated_vec)
-#x2, y2 = m.change_to_vector(decimated_vec2)
+coefficients = np.polyfit(df_preprocessed["Czas"], df_preprocessed["CH1_filtered"], 1)
+slope = coefficients[0]
+intercept = coefficients[1]
+regression_line = slope * df_preprocessed["Czas"] + intercept
 
-#test = decimated_vec.head(-1)
-#test2 = decimated_vec2.head(-1)
-
-plt.scatter(df_preprocessed["Czas"], df_preprocessed["CH1_filtered"], label='L')
-#plt.scatter(test2["Czas"], test2["CH1"], label='P')
-#plt.plot(x2, y2, 'r.', label='P')
+plt.scatter(df_preprocessed["Czas"], df_preprocessed["CH1_filtered"], label='L', color='blue')
+plt.plot(df_preprocessed["Czas"], regression_line, color='purple', label='Regression')
 plt.title("Przebiegi parami")
 plt.xlabel("Czas")
 plt.ylabel("Wartości pomiarów")
-#plt.ylim(0, 1)
-#plt.xlim(-2, 6)
-#plt.yticks(np.arange(0, 0.1, 100))
 plt.legend()
 plt.show()
 
-
-#m.regression_model(test)
-# wektor z wektorow
-# vectors = []
-# for i in range(len(x1)):
-#   vec = [x1[i], y1[i]]
-#  vectors.append(vec)
-#  print(vec)
-
-
-# print("Original data shape: ", test.shape)
-# print("Decimated data shape: ", decimated_vec.shape)
-# print(decimated_vec)
