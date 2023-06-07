a0e761326c3991abdac74f9af9616c7257a02881
magddzi881 <72300027+magddzi881@users.noreply.github.com>
2023-06-07 19:41:15 +0200

    Testy

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -6,9 +6,10 @@ from scipy.fft import fft
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
 from tensorflow import keras
 
-# wczytuje dane do jednego datasetu, pozdrawiam Ewunie
 data_files = ['measurements/Tek0000.csv', 'measurements/Tek0001.csv',
               'measurements/Tek0002.csv', 'measurements/Tek0003.csv',
               'measurements/Tek0004.csv', 'measurements/Tek0005.csv',
@@ -41,8 +42,31 @@ parametry2 = {
     'dynamika_zmian': okres['CH2'].max() - okres['CH2'].min()
 }
 print(parametry1)
-print(parametry2)
 
+# Tworzenie ramki danych na podstawie parametrów
+data = pd.DataFrame([parametry1, parametry2])
+
+# Podział danych na cechy (X) i etykiety (y)
+X = data.drop(['wartosc_srednia'], axis=1)  # Usuń kolumnę 'wartosc_srednia' jako etykietę
+y = data['wartosc_srednia']
+
+# Podział danych na zbiór treningowy i testowy
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Inicjalizacja i uczenie modelu sieci neuronowej
+model = MLPRegressor(hidden_layer_sizes=(100, 50), activation='relu', random_state=42)
+model.fit(X_train, y_train)
+
+# Ocena modelu na zbiorze testowym
+score = model.score(X_test, y_test)
+print("R^2 score:", score)
+
+# Przykładowe przewidywanie na nowych danych
+new_data = pd.DataFrame([
+    {'okres_t': 10, 'odchylenie': 0.5, 'mediana': 5, 'min': 0, 'max': 10, 'srednia_harmoniczna': 3, 'dynamika_zmian': 10}
+])
+prediction = model.predict(new_data)
+print("Przewidywana wartość średnia:", prediction)
 
 
 # old main -------------------------------------------------------------------------------------------------------------
